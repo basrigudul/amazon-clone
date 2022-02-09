b3211786194690498daaf6d775bf36009fd12f16
Önder Cem Hazir <onderhazir@yahoo.com>
2022-02-09 17:00:43 +0100

    Home Page design completed

---
+++ src/Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Home.css'
+import Product from './Product'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='home__container'>
+            <img 
+            className='home__image'
+            src="https://m.media-amazon.com/images/G/03/digital/video/mobileprog/appLPs/UK_MAI_MobileApp_LPC_719x414._CB1198675309_.jpg" />
+        
+
+            <div className='home__row'>
+                <Product 
+                    title="The lean startup"
+                    price={29.99}
+                    image="https://images-na.ssl-images-amazon.com/images/I/51PU3tU3BfL._SX397_BO1,204,203,200_.jpg"
+                    rating={5}
+                />
+                <Product />
+            </div>
+
+            <div className='home__row'>
+                <Product />
+                <Product />
+                <Product />
+            </div>
+
+            <div className='home__row'>
+                <Product />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9prsNFO6adaXxHdBYlokju7I131X8V8A",
+    authDomain: "clone-adba4.firebaseapp.com",
+    projectId: "clone-adba4",
+    storageBucket: "clone-adba4.appspot.com",
+    messagingSenderId: "759490671397",
+    appId: "1:759490671397:web:eaea5608f1e8d40b2f20cc"
+  };
