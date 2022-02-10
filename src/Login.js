dbd2477a4d8389620ee4d87bc95e9dc50fe445be
Önder Cem Hazir <onderhazir@yahoo.com>
2022-02-10 21:27:47 +0100

    Front-end of sign in page completed.

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Login.css'
+import {Link } from 'react-router-dom'
 
 function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const signIn = (e) => {
+    e.preventDefault()
+    
+    // some firebase login stuff
+  }
+
+  const register = (e) => {
+    e.preventDefault() 
+
+  }
+
   return (
-    <div>Login</div>
+    <div className='login'>
+      <Link to='/'>
+        <img className='login__logo' src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" />
+      </Link>
+
+      <div className='login__container'>
+        <h1>Sign in</h1>
+        <form>
+          <h5>Email</h5>
+          <input type='text' value={email} onChange= {e => setEmail(e.target.value) }/>
+
+          <h5>Password</h5>
+          <input type='password' value={password} onChange= {e => setPassword(e.target.value) }/>
+
+          <button className='login__signInButton' onClick={signIn}>Sign In</button>
+        </form>
+
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+        </p>
+
+        <button className='login__registerButton' onClick={register}>Create your Amazon Account</button>
+      </div>
+    </div>
   )
 }
 
